6815b325f806372d0de5f60f261a20e2cba519d2
Vadim <vadimbaranivsky83@gmail.com>
2021-07-13 17:45:07 +0300

    t06 greeting capitalize quick fix

--- t06/js/script.js
+++ t06/js/script.js
@@ -1,9 +1,13 @@
+
 (greet = () => {
-    const firstName = prompt('Enter first name')
-    const lastName = prompt('Enter last name')
+    String.prototype.capitalize = function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    }
+    const firstName = prompt('Enter first name').capitalize()
+    const lastName = prompt('Enter last name').capitalize()
     let message
     if (firstName && lastName && isNaN(firstName) && isNaN(lastName)) {
-        message = `Hello, ${firstName} ${lastName}`
+        message = `Hello, ${firstName.capitalize()} ${lastName.capitalize()}`
     } else {
         message = 'Wrong input!'
     }
