ec8e746e2dddc85ce8d6417bd5133a58647f3590
Vadim <vadimbaranivsky83@gmail.com>
2021-09-11 17:11:29 +0300

    errors and success messages better; little refactor

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,36 +1,21 @@
 import jwt from 'jsonwebtoken'
 import {promisify} from 'util'
 
-export const signToken=  id =>{
 
-    return jwt.sign({id},   process.env.JWT_SECRET,{
+  
+ export  const addJwtCookie = (res, value) =>{
+    const token = jwt.sign({id: value},   process.env.JWT_SECRET,{
       expiresIn: process.env.JWT_SECRET_IN
-    });
-  
-  }
-  
- export  const createAndSendToken= (user, statusCode, res, payload) =>{
-    const token= signToken(user._id);
+    })
+
     const cookieOptions= {
       expires: new Date(Date.now()+process.env.JWT_COOKIES_EXPIRES_IN*24*3600*1000),
       httpOnly: true
     }
   
-    if(process.env.NODE_ENV=== 'production') cookieOptions.secure=true
+    if(process.env.NODE_ENV=== 'production') cookieOptions.secure = true
   
     res.cookie('jwt', token, cookieOptions);
-  
-    user.password=undefined;
-  
-    res.status(statusCode).json({
-      status: 'success',
-      token,
-      data: {
-        user
-      },
-      ...payload
-    })
-  
   }
 
   export async function jwtParser(req, res, next) {
@@ -39,7 +24,7 @@ export const signToken=  id =>{
       token= req.cookies.jwt
     }
     if(!token){
-      res.send('cookies doesnt contain token')
+      return res.send('cookies doesnt contain token')
     }
     req.decoded=await promisify(jwt.verify)(token, process.env.JWT_SECRET);
     next()
