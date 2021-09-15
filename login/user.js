0881699c8f79b19cdf2e975c0d3d596951956c85
Artem Nikoliuk <90250166+anikoliuk@users.noreply.github.com>
2021-09-15 19:34:59 +0300

    Delete login directory

--- login/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, default: null },
-  password: { type: String },
-  token: { type: String },
-});
-
-module.exports = mongoose.model("user", userSchema);
